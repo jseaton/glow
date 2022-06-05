eed52cab775e890029d15098a87dbfd0fa002f5a
Jo Seaton <js-git@sigmanetwork.org>
2022-06-05 16:40:25 +0100

    File listing in web UI + git commits

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 import subprocess
 import re
 import tempfile
+import os
 
 app = Flask(__name__)
 
@@ -15,25 +16,32 @@ def root():
 vec4 mainImage() {
     return vec4(sin(iTime), 0., 0., 1.);
 }"""
+    filename = request.args.get('filename', default='changeme.glsl')
     if request.method == 'POST':
         shader = request.form['data']
         filename = request.form['filename']
-        m = re.match(r'^([a-z0-9][a-z0-9-]+)\.glsl$', filename)
-        assert m
-        head = m.groups()[0]
 
-        glsl  = f"shaders/{head}.glsl"
-        temp  = f"temp/{head}.spv"
-        final = f"compiled/{head}.spv"
+    m = re.match(r'^([a-z0-9][a-z0-9-]+)\.glsl$', filename)
+    assert m
+    head = m.groups()[0]
 
+    glsl  = f"shaders/{head}.glsl"
+    temp  = f"temp/{head}.spv"
+    final = f"compiled/{head}.spv"
+
+    if request.method == 'POST':
         with open(glsl, "w") as f:
-            f.write(request.form['data'])
+            data = request.form['data'].replace('\r', '')
+            f.write(data)
 
         page = "<p>submitted, result:</p>" + page
 
         state = subprocess.run(f"cat prefix.glsl {glsl} postfix.glsl | glslangValidator -V --stdin -S frag -o {temp}", shell=True, capture_output=True, text=True)
 
         if state.returncode == 0:
+            subprocess.run(["git", "add", glsl])
+            subprocess.run(["git", "commit", "-m", "auto commit", glsl])
+
             subprocess.run(["mv", temp, final])
 
             with open("current", "w") as current:
@@ -42,7 +50,12 @@ vec4 mainImage() {
             page = "<p>Success!</p>" + page
         else:
             page = f"Failure {state.returncode}: <pre>{state.stderr}</code><pre>{state.stdout}</code>{page}"
-        
-    form = f"<form action='/' method='post'><div><label for='filename'>Filename</label><input type='text' value='changeme.glsl' name='filename' /></div><div><textarea name='data' cols='80' rows='40'>{shader}</textarea></div><input type='submit' /></form>"
 
-    return "<h1>Shader Dome</h1>" + page + form + intro
+    with open(glsl, "r") as g:
+        shader = g.read()
+        
+    form = f"<form action='/' method='post'><div><label for='filename'>Filename</label><input type='text' value='{filename}' name='filename' /></div><div><textarea name='data' cols='80' rows='40'>{shader}</textarea></div><input type='submit' /></form>"
+
+    listing = "<ul>" + "".join([f"<li><a href='/?filename={f}'>{f}</a></li>" for f in os.listdir("shaders")]) + "</ul>"
+
+    return "<h1>Shader Dome</h1>" + page + form + intro + listing
